e6bd3bdc3f673ebafe13f66a39123c369abcb952
Colin Lacy <colacy@cisco.com>
2026-02-01 16:47:08 -0500

    makes style updates

--- apps/public-app/src/App.jsx
+++ apps/public-app/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="app">
       <img src={gatewayImage} alt="Gateway all the things" className="gateway-image" />
-      <p className="welcome-text">Welcome to the Envoy community!</p>
+      <p className="welcome-text">Welcome to the <span>Envoy</span> community!</p>
     </div>
   )
 }
